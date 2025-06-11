05ceb4c2f8a88b61e5096fcf2acbdf739547ce14
Andrew <andrewjnaimou@yahoo.com>
2025-06-11 10:07:49 -0400

    Gates now reference its targets so they invoke there Dead events. Enemy moves to idle as expected.

--- Assets/Scripts/Defend the Gates/Managers/Gate.cs
+++ Assets/Scripts/Defend the Gates/Managers/Gate.cs
@@ -8,8 +8,11 @@ namespace Etheral
     {
         [Header("Gate References")]
         [SerializeField] List<DefensesHealth> defensesHealth;
+        [SerializeField] List<DefenseTarget> defensesTargets;
         [SerializeField] GameObject gateVisuals;
 
+        
+        //Used to update Enemy Moving Towards Gate
         public event Action<Gate> OnGateDestroyed;
         public bool IsDestroyed { get; private set; }
 
@@ -29,6 +32,11 @@ namespace Etheral
             IsDestroyed = true;
             gateVisuals.SetActive(false);
             OnGateDestroyed?.Invoke(this);
+            
+            foreach (var target in defensesTargets)
+            {
+                target.HandleDeadTarget();
+            }
         }
 
 
